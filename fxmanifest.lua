f259a9064e5d906ae9c9eabd329aeecd4e5333ec
fivebazaar <165402122+fivebazaar@users.noreply.github.com>
2025-09-20 16:14:44 +0300

    version 1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Leave Engine Running - Modern & Optimized'
+version '3.0.0'
+author 'FAXES - Optimized by alpy'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+dependencies {
+    'ox_lib'
+}
+
+lua54 'yes'
